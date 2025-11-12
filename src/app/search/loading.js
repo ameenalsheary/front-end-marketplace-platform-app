aa04bd56dea04727289a9ba0b4e4d51bf5a13236
Ameen Al-sheary <95883429+ameenalsheary@users.noreply.github.com>
2025-11-12 13:17:36 +0000

    Change skeleton of search page.

--- src/app/search/loading.js
+++ src/app/search/loading.js
@@ -1,9 +1,7 @@
-import LoadingIcon from "@/components/ui/loadingIcon/LoadingIcon";
+import ProductGridSkeleton from "@/components/product/ProductGridSkeleton";
 
 export default function Loading() {
   return (
-    <div className="bg-background-secondary w-full h-screen-minus-header rounded-lg flex justify-center items-center">
-      <LoadingIcon />
-    </div>
+    <ProductGridSkeleton />
   );
 }
